f9945e9a1f3258fbed4b75dc55319bb3155ff5f7
Tit Kovalenko <t.a.kvlnk@gmail.com>
2024-01-11 00:29:26 +0200

    feat: dirty version which updates the gsheet but uses primitive auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import './App.css';
+
+const CLIENT_ID = '905474591291-suogt2amt9sqlop15te377an0ugbc7f9.apps.googleusercontent.com';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [authToken] = useState(() => new URLSearchParams(window.location.hash.slice(1)).get('access_token'))
+  const [isReady, setIsReady] = useState(false)
+  const [googleUser, setGoogleUser] = useState<null | unknown>(null)
+
+  useEffect(() => {
+    if (!authToken) {
+      setIsReady(true);
+      return;
+    }
+
+    
+    fetchGoogleUser(authToken).then(setGoogleUser).finally(() => setIsReady(true));
+  }, [authToken])
+
+  if (!isReady) return <div>Loading...</div>;
+
+  if (googleUser) {
+    return (
+      <div>
+      <code>{JSON.stringify(googleUser, null, 2)}</code>
+      <button onClick={async () => {
+        await (await gSheetsClient).spreadsheets.values.update({
+          access_token: authToken!,
+          spreadsheetId: '1TcpjYSMU6_AegrKyCfYJvcuTcWdzEKT1dHpgScGEo2Q',
+          range: 'WebDemo!C5',
+          valueInputOption: 'USER_ENTERED',
+          resource: {
+            values: [['hello world1111']]
+          }
+        });
+
+        alert('done');
+      }}>edit table</button>
+      </div>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button onClick={authorizeGoogle}>
+      Sign in via google
+    </button>
   )
 }
 
 export default App
+
+function authorizeGoogle() {
+  const url = new URL('https://accounts.google.com/o/oauth2/v2/auth');
+  url.searchParams.append('scope', ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'].join(' '));
+  url.searchParams.append('client_id', CLIENT_ID);
+  url.searchParams.append('redirect_uri', 'https://localhost:3000');
+  url.searchParams.append('response_type', 'token');
+
+  window.location.href = url.toString()
+}
+
+async function fetchGoogleUser(authToken: string) {
+  const res = await fetch('https://www.googleapis.com/oauth2/v2/userinfo?alt=json', {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+
+  return res.json();
+}
+
+const gSheetsClient = new Promise<typeof gapi.client.sheets>((resolve) => {
+  
+  gapi.load('client:auth2', initClient);
+
+  async function initClient() {
+    console.log('initClient');
+
+    await gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4');
+
+    console.log('shhets', (gapi.client).sheets)
+
+    resolve((gapi.client).sheets);
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  server:{
+    port: 3000,
+    host: '127.0.0.1',
+    https: true
+  }
 })
